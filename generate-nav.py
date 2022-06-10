cab4dc763f58f8a0ce0bb51a624dad1104b88a59
Alan Roth <alan.roth@payara.fish>
2022-06-10 13:42:23 +0100

    Refactored script to include distribution only files also. Added Documentation.

--- generate-nav.py
+++ generate-nav.py
@@ -20,7 +20,7 @@ DOCS_PREFIX = "docs/modules/ROOT/"
 PAGES_PREFIX = DOCS_PREFIX + "pages/"
 
 FILE_EXTENSION="adoc"
-OVERVIEW_FILE_NAME="Overview.{extension}".format(extension=FILE_EXTENSION)
+OVERVIEW_FILE_NAME="Overview.adoc"
 
 NAV_PATH = DOCS_PREFIX + "nav.adoc"
 
@@ -32,64 +32,118 @@ PARTIALS = {"Jakarta EE Certification":"jakarta-ee.adoc",
 
 ### Helpers ###
 
-def remove_substring(value:str, substring:str) -> str:
-    return str(value).replace(substring, "")
+def remove_substring(string:str, substring:str) -> str:
+    '''Removes a provided substring from a string. Returns the result.'''
+
+    return str(string).replace(substring, "")
 
 
-def remove_substrings(value:str, substrings) -> str:
-    output = value
+def remove_substrings(string:str, substrings) -> str:
+    '''Removes a list of substrings from a single string. Returns the result.'''
+
+    output = string
     for substring in substrings:
         output = remove_substring(output, substring)
     return output
 
 
 def make_xref(depth:int, file:str) -> str:
+    '''Creates a formatted Xref line from the filepath and filename.'''
+
     file_name = get_name_from_path(file)
     file_name = remove_substring(file_name, ".{0}".format(FILE_EXTENSION))
     return "{fdepth} xref:{fpath}[{fname}]".format(fdepth=depth*"*", fpath=file, fname=file_name)
 
 
 def make_xref_unlinked(depth:int, name:str) -> str:
+    '''Makes a formatted unlinked Xref line from the filepath and filename.'''
+
     return "{ddepth} {dname}".format(ddepth=depth*"*", dname=get_name_from_path(name))
 
-def get_name_from_path(value:str) -> str:
-    value = value.rpartition(os.path.sep)
-    return value[len(value)-1]
+
+def get_name_from_path(string:str) -> str:
+    '''Divides the filepath and returns only the filename.'''
+
+    return get_path_subsection(string, 1)
+
+
+def get_dir_up(string:str) -> str:
+    '''Returns the next directory up in the file path.'''
+
+    return get_path_subsection(string, 3)
+
+
+def get_path_subsection(string:str, index:int) -> str:
+    '''Divides the filepath string based on the os.path.sep value. Returns the value at the requested index.'''
+
+    subsection = string.rpartition(os.path.sep)
+    return subsection[len(subsection)-index]
+
 
 def get_depth(file_path:str) -> int:
-    return file_path.count(os.path.sep)
+    '''Counts the number of os.path.sep, which will be the depth of the file in the file structure.'''
 
+    return file_path.count(os.path.sep)
 
 ### Functions ###
 
-def gen_nav(parent:str, distribution:str) -> list:
-    output = []
-    #Bool value to determine if the parent directory exists only in the distribution specific documentation
-    if(not os.path.exists(parent)):
-        parent = os.path.join(distribution, parent)
-    
-    for dir, subdirs, files in os.walk(parent, topdown=True):
-        relative_dir = remove_substrings(dir, DISTRIBUTIONS + [PAGES_PREFIX])
+def get_tree(parent:str) -> dict:
+    '''Returns a depth-first tree of the directory path as the key, and the list of files as the value. Parent is the root of the tree.'''
 
-        logging.debug("Relative Directory {0}".format(relative_dir))
+    tree = {}
 
-        dirstribution_dir_path = os.path.join(distribution, dir)
-        if(os.path.exists(dirstribution_dir_path)):
-            files += os.listdir(dirstribution_dir_path)
-
-        logging.debug("Files {0}".format(files))
-
-        #Avoid writing root title as unlinked xref
-        if(dir is not parent):
-            output.append(make_xref_unlinked(get_depth(relative_dir), relative_dir))
-        if(files):
-            #Put all Overview files to the beginning order
-            if OVERVIEW_FILE_NAME in files:
+    for root, _, files in os.walk(parent, topdown=True):
+        if(files and OVERVIEW_FILE_NAME in files):
+            index = files.index(OVERVIEW_FILE_NAME)
+            if(index != 0):
+                logging.debug("Moved Overview file for %s to the top of the directory", root)
                 files.insert(0, files.pop(files.index(OVERVIEW_FILE_NAME)))
-            for file in files:
-                if file.endswith(FILE_EXTENSION):
-                    joint_path = os.path.join(relative_dir, file)
-                    output.append(make_xref(get_depth(joint_path), joint_path))
+        tree[root] = files
+
+    return tree
+
+def gen_nav(parent:str, distribution:str):
+    '''Creates two trees, one for the base documentation and another for the distribution only documentation.
+    It attempts to insert the distribution only files into the base documentation, if that is not possible it will move up one 
+    directory until it finds a shared file path. Otherwise it will print at the same path the file exists in the distribution specific documentation, as 
+    it is then considered unique to the distribution and should be included.
+    
+    It then generates the Xref values to be printed into the nav.'''
+
+    output = []
+
+    tree = get_tree(parent)
+    distribution_tree = get_tree(os.path.join(distribution, parent))
+
+    key_list = [key for key in tree]
+    
+    for key in distribution_tree:
+        agnostic_path = remove_substring(key, distribution)
+        if agnostic_path in key_list:
+            continue
+        
+        dir_up = agnostic_path
+        while(dir_up and dir_up not in key_list):
+            dir_up = get_dir_up(dir_up)
+
+        if(dir_up):
+            key_list.insert(key_list.index(dir_up)+1, agnostic_path)
+            continue
+
+        key_list.append(agnostic_path)
+                
+    for key in key_list:
+        relative_path = remove_substrings(key, [PAGES_PREFIX, distribution])
+        if(parent != key):
+            output.append(make_xref_unlinked(get_depth(relative_path), key))
+        if(key in tree):
+            for value in tree[key]:
+                output.append(make_xref((get_depth(relative_path)+1), os.path.join(relative_path, value)))
+        
+        distribution_key = os.path.join(distribution, key)
+        if(distribution_key in distribution_tree):
+            for value in distribution_tree[distribution_key]:
+                output.append(make_xref((get_depth(relative_path)+1), os.path.join(relative_path, value)))
 
     return output
 
@@ -97,12 +151,12 @@ if __name__ == "__main__":
     for distribution in DISTRIBUTIONS:
         nav = {}
 
-        logging.debug("Beginning Generation for {0}".format(distribution))
+        logging.debug("Beginning Generation for %s", distribution)
 
         #Virtually combine directory trees for the base documentation + distribution documentation
         root_dirs = set(os.listdir(os.path.join(distribution, PAGES_PREFIX))).symmetric_difference(PARTIALS).union(os.listdir(PAGES_PREFIX))
 
-        logging.debug("Root directories {0}".format(root_dirs))
+        logging.debug("Root directories %s", root_dirs)
 
         #Loop through the top level directories
         for set_dir in root_dirs:
@@ -113,23 +167,23 @@ if __name__ == "__main__":
         NAV_LOCATION = os.path.join(distribution, NAV_PATH)
         
         #Clear file contents
-        with open(NAV_LOCATION, 'w') as f:
+        with open(NAV_LOCATION, 'w', encoding="utf-8") as f:
             pass
 
-        with open(LAYOUT_FILE) as layout:
+        with open(LAYOUT_FILE, encoding="utf-8") as layout:
             for value in layout.readlines():
                 value = value.strip()
-                with open(NAV_LOCATION, 'a') as nav_file:
+                with open(NAV_LOCATION, 'a', encoding="utf-8") as nav_file:
                     if value in PARTIALS:
-                        nav_file.write("\ninclude::partial${partial}[]\n".format(partial=PARTIALS[value]))
-                        logging.info("{0} value was replaced by a Partial file in the {1} nav.".format(value, distribution))
+                        nav_file.write(f"\ninclude::partial${value}[]\n")
+                        logging.info("%s value was replaced by a Partial file in the %s nav.", value, distribution)
                         continue
 
                     if value not in nav:
-                        logging.warning("{0} value was not found in nav. It was skipped instead.".format(value))
+                        logging.warning("%s value was not found in nav. It was skipped instead.", value)
                         continue
                     
-                    nav_file.write( "\n.{title}\n".format(title=value))
+                    nav_file.write(f"\n.{value}\n")
                     
                     for line in nav[value]:
                         if IS_WINDOWS:
